cd119315ad6bae1a143bbebee1cf9ab3ddab9ee2
Nigel Horne <njh@bandsman.co.uk>
2025-09-04 21:09:34 -0400

    Try again with the sort

--- scripts/generate_index.pl
+++ scripts/generate_index.pl
@@ -286,7 +286,7 @@ foreach my $file (@history_files) {
 
 my $js_data = join(",\n", @data_points);
 
-@data_points = sort { $a->{'x'} <=> $b->{'x'} } @data_points;
+@data_points = sort { $a cmp $b } @data_points;
 
 if(scalar(@data_points)) {
 	$html .= <<'HTML';
